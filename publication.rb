e97f8916b8388546af81845dc39f609dbcd7b2ec
Alice Bartlett <ab@berglondon.com>
2012-03-14 10:56:40 +0000

    added etag. Fixed pull

--- publication.rb
+++ publication.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'json'
 require 'rest-client'
-
+require './word_of_the_day'
+require 'date'
 post '/validate_config/' do
   content_type :json
   response = {}
@@ -17,26 +18,40 @@ get '/sample/' do
   erb :word_of_the_day
 end
 
-#TODO::AB POKE ME WITH YOUR CRON JOB
-#TODO::AB REMOVE REFS TO LOCAL HOST
+get '/edition/' do
+  success = false
+  tries = 0
+  while !success && tries < 3
+    tries +=1
+    begin
+      @word, @definition = WordOfTheDay.fetch_source
+      success = true
+    rescue Exception => e
+      # Parse problem. Not going to go away. Do someting
+        
+      if tries == 3
+        etag Digest::MD5.hexdigest("")
+        raise NotableError, 'caught a parse error 3 times. Last error message: '+ e.message 
+      end 
+    end
+  end
+  etag Digest::MD5.hexdigest(@word+Date.ordinal.to_s)
+  erb :word_of_the_day
+end
+
+
 def cronable
-  require './word_of_the_day'
-  @wotd = WordOfTheDay.new
-  
-  
+
   success = false
   tries = 0
    
   while !success && tries < 3
     tries +=1
     begin
-      result = @wotd.word_changed?
-      if result
-        RestClient.post("http://staging.bergcloud.com/api/v1/publications/renders", :config => {}, :html => render_html, :developer_key => 'a94d7051b1b93e39451e653179cac8ae', :endpoint => 'http://empty-stream-2306.herokuapp.com/')
-      end
+      RestClient.post("http://staging.bergcloud.com/api/v1/publications/renders", :config => {}, :html => render_html, :developer_key => 'a94d7051b1b93e39451e653179cac8ae', :endpoint => 'http://bergcloud-wordoftheday.herokuapp.com/')
       success = true
     rescue PermanentError => e
-      # Parse problem. Not going to go away. Do someting #TODO AB Make this error notify by email or something
+      # Parse problem. Not going to go away. Do someting
       raise NotableError, 'caught a parse error 3 times. Last error message: '+ e.message if tries == 3
     rescue NetworkError
       # Network error. Do nothing.
@@ -46,7 +61,7 @@ def cronable
 end
 
 def render_html
-  bind_me = BindMe.new(@wotd.current_word, @wotd.current_definition)
+  bind_me = BindMe.new(WordOfTheDay::fetch_source)
   rhtml = ERB.new(File.read('views/word_of_the_day.erb'))
   rhtml.result(bind_me.get_binding)
 end
--- word_of_the_day.rb
+++ word_of_the_day.rb
@@ -2,44 +2,10 @@ class WordOfTheDay
   require 'nokogiri'
   require 'open-uri'
   
-  WORD_FILE = 'wotd.json'
   CONTENT_URL = "http://wordsmith.org/awad/rss1.xml"
-  SAMPLE_DATA = {:word => "ubiqitous", :definition => "Being or seeming to be everywhere at the same time."}
-  
-  def initialize
-    if !File.exists?(WORD_FILE)
-      word, definition = fetch_source
-      set_word word, definition
-    end
-    @wotd = JSON.parse (IO.read(WORD_FILE))
-  end
-  
-  def word_changed?
-    new_word, new_definition = fetch_source
-    
-    # Update if new content
-    if @wotd['word'] != new_word
-      set_word new_word, new_definition
-      return true
-    end
-    return false
-  end
-  
-  def set_word word, definition
-   contents = {"definition" => definition, "word"=>word}.to_json
-   File.open(WORD_FILE, 'w+') {|f| f.write(contents) }
-   @wotd = JSON.parse (IO.read(WORD_FILE))
-  end
-  
-  def current_word
-    @wotd["word"]
-  end
-  
-  def current_definition
-    @wotd["definition"]
-  end
-  
-  def fetch_source      
+  SAMPLE_DATA = {:word => "ubiquitous", :definition => "Being or seeming to be everywhere at the same time."}
+
+  def self.fetch_source      
     begin
       doc = Nokogiri.parse(open(CONTENT_URL))
     rescue OpenURI::HTTPError
